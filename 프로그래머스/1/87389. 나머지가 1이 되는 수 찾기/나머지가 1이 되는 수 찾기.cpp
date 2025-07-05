f0e0bbb688eaa43c1d7b8056560569883ff414de
BYUN-HYUK <hyuk990504@gmail.com>
2025-07-05 16:13:46 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 2.25 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int x = 1; x < n; ++x) 
+    {
+        if(n % x == 1) 
+        {
+            return x;
+        }
+    }
+    
+    
+    return answer;
+}
