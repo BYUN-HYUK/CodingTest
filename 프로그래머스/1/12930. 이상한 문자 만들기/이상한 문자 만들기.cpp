0d8d6354f2f75f0b129ad550794188233044c528
BYUN-HYUK <hyuk990504@gmail.com>
2025-08-11 20:55:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int word_index = 0;
+    
+    for (char c : s) { 
+        if (c == ' ') {
+            word_index = 0;
+            answer += ' ';
+        } else {
+            if (word_index % 2 == 0) {
+                answer += toupper(c);
+            } else {
+                answer += tolower(c);
+            }
+            word_index++;
+        }
+    }
+    
+    return answer;
+}
